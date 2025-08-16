86b236778a6a2574cf2d7fe076efba44fbaa3fd6
sjmitch-git <sjmitch20@outlook.com>
2025-08-16 20:42:39 +0100

    Adds github actions

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   moduleDirectories: ["node_modules", "dist/lib/es6"],
   testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
 };
